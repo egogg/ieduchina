24493d130ad2fd1444b2ee88503400b4fcbcdfc6
egogg <egogg@163.com>
2018-08-06 22:57:03 +0800

    add time delay

--- ieduchina/spiders/articles.py
+++ ieduchina/spiders/articles.py
@@ -8,6 +8,7 @@ from scrapy.linkextractors import LinkExtractor
 import re
 import os
 from selenium import webdriver
+from time import sleep
 
 class ArticlesSpider(CrawlSpider):
 	name = 'articles'
@@ -29,6 +30,8 @@ class ArticlesSpider(CrawlSpider):
 		item_links = response.css('.article_list_con .article_item .article_info h4 a::attr(href)').extract()
 		for a in item_links:
 			print('    visiting: ' + a[2:])
+			sleep(30)
 			print('    visiting: ' + 'm.' + a[2:])
+			sleep(30)
 		
 	parse_start_url = parse_item
