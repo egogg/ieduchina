55ee143866d317e4096d2029726a51eec3139508
egogg <egogg@163.com>
2018-10-19 13:59:29 +0800

    generate postman collections

--- ieduchina/spiders/articles.py
+++ ieduchina/spiders/articles.py
@@ -22,6 +22,7 @@ class ArticlesSpider(scrapy.Spider):
 	article_id_parttern = re.compile(r'^\/\/.*\/([0-9]+).html$')
 
 	urls = []
+	article_ids = []
 
 	def __init__(self, mode = '0', *args, **kwargs):
 		super(ArticlesSpider, self).__init__(*args, **kwargs)
@@ -70,6 +71,7 @@ class ArticlesSpider(scrapy.Spider):
 						
 						self.urls.append(pc_request_url)
 						self.urls.append(m_request_url)
+						self.article_ids.append(article_id)
 
 			formdata = {
 				'page': str(page + 1),
@@ -80,6 +82,71 @@ class ArticlesSpider(scrapy.Spider):
 			return
 
 	def closed( self, reason ):
-		print("end")
-		print(self.urls)
+		generate_postman_collection(self.article_ids)
+
+
+	def build_postman_item(article_id, pc_mode) :
+		counter_params = 'op=count&id=' + article_id + '&modelid=1'
+		if(pc_mode) :
+			request_url = self.pc_counter_url + counter_params
+		else:
+			request_url = self.m_counter_url + counter_params
+		item = {
+			"name": article_id,
+			"request": {
+				"method": "GET",
+				"header": [],
+				"body": {
+					"mode": "raw",
+					"raw": ""
+				},
+				"url": {
+					"raw": request_url,
+					"protocol": "https",
+					"host": [
+						"www",
+						"ieduchina",
+						"com"
+					],
+					"path": [
+						"api.php"
+					],
+					"query": [
+						{
+							"key": "op",
+							"value": "count"
+						},
+						{
+							"key": "id",
+							"value": article_id
+						},
+						{
+							"key": "modelid",
+							"value": "1"
+						}
+					]
+				}
+			},
+			"response": []
+		}
+
+		return item
+
+	def generate_postman_collection(article_ids) :
+		items = []
+		for article_id in article_ids :
+			items.append(build_postman_item(article_id, True));
+			items.append(build_postman_item(article_id, False));
+
+		data = {
+			"info" : {
+				"_postman_id": "d38cfd79-91d5-42d9-a8e4-95a33bd1ec4b",
+				"name": "ieduchina_collection",
+				"schema": "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+			},
+			"item" : items
+		}
+
+		with open('ieduchina_collection.json', 'w') as outfile:
+			json.dump(data, outfile)
 	
